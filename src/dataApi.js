a1cadb6164e55b421b9b163ca03329b41d93b8b0
taoufik chahouat <taoufik.chahouat@gmail.com>
2022-09-26 14:53:55 +0200

    final version API service

--- src/dataApi.js
+++ src/dataApi.js
@@ -1,6 +1,8 @@
 import { USER_MAIN_DATA, USER_ACTIVITY, USER_AVERAGE_SESSIONS, USER_PERFORMANCE } from './data'; 
 import axios from 'axios';
 
+let dataMock=false
+
 let getDataBackEnd = async (url) => {
     try {
       let res = await axios.get(url).then(res => res.data.data);
@@ -12,9 +14,20 @@ let getDataBackEnd = async (url) => {
     }
 }
 
+async function getData() {
+    if (await getDataBackEnd("http://localhost:3001/user/"+12) === false) {
+        dataMock = true
+    }   
+}
+getData();
+
 async function getUserMainData(id) {
-    let userMainData = await getDataBackEnd("http://localhost:3001/user/"+id);
-    if (userMainData === false){
+    let userMainData;
+    if (dataMock === false) {
+        userMainData = await getDataBackEnd("http://localhost:3001/user/"+id);
+    }
+    if (dataMock === true){
+        console.log(userMainData);
         USER_MAIN_DATA.find((item)=>{
             if (id == item.id) {
                 userMainData = item
@@ -23,14 +36,17 @@ async function getUserMainData(id) {
         })
         return userMainData
     }
-     else if (userMainData != undefined) {
+    else if (userMainData != undefined) {
         return userMainData;
     }
 }
 
 async function getUserActivity(id) {
-    let userActivity = await getDataBackEnd("http://localhost:3001/user/"+id+"/activity");
-    if (userActivity === false) {
+    let userActivity;
+    if (dataMock === false) {
+        userActivity = await getDataBackEnd("http://localhost:3001/user/"+id+"/activity");   
+    }
+    if (dataMock === true) {
         USER_ACTIVITY.find(item => {
             if (id == item.id || id == item.userId) {
                 userActivity = item
@@ -45,15 +61,17 @@ async function getUserActivity(id) {
 }
 
 async function getUserAverageSessions(id) {
-    let userAverageSession = await getDataBackEnd("http://localhost:3001/user/"+id+"/average-sessions");
-    if (userAverageSession === false) {
+    let userAverageSession;
+    if (dataMock === false) {
+        userAverageSession = await getDataBackEnd("http://localhost:3001/user/"+id+"/average-sessions");   
+    }
+    if (dataMock === true) {
         USER_AVERAGE_SESSIONS.find(item => {
             if (id == item.id || id == item.userId) {
                 userAverageSession = item
                 return userAverageSession
             }
         })
-        console.log(userAverageSession);
         return userAverageSession
     }
     else if (userAverageSession != undefined) {
@@ -62,8 +80,11 @@ async function getUserAverageSessions(id) {
 }
 
 async function getUserPerformance(id) {
-    let userPerformance = await getDataBackEnd("http://localhost:3001/user/"+id+"/performance");
-    if (userPerformance === false) {
+    let userPerformance;
+    if (dataMock === false) {
+        userPerformance = await getDataBackEnd("http://localhost:3001/user/"+id+"/performance");   
+    }
+    if (dataMock === true) {
         USER_PERFORMANCE.find(item => {
             if (id == item.id || id == item.userId) {
                 userPerformance = item
